6fdeedc1800a1b18d53c788192eda182a51ea526
Sebastien <sebastienfct@gmail.com>
2018-07-05 21:14:27 -0500

    Testing CI

--- ios/Meetup-Project/Meetup Project/Controllers/HomeVC.swift
+++ ios/Meetup-Project/Meetup Project/Controllers/HomeVC.swift
@@ -1,25 +1,21 @@
 //
-//  ViewController.swift
+//  HomeVC.swift
 //  Meetup Project
 //
-//  Created by sebastien FOCK CHOW THO on 2018-06-21.
+//  Created by sebastien FOCK CHOW THO on 2018-07-05.
 //  Copyright © 2018 sebastien FOCK CHOW THO. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
 }
-
